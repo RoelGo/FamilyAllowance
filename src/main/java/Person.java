c19444e0cb90ba490509f67bddca35abb04629d8
Roel Goossens <roelgoossens22@hotmail.com>
2017-01-18 11:05:42 +0100

    Changed instance variables
    private doesn,t  allows you to acces to it from a subclass

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,4 @@
 package main.java;
-
 import java.time.LocalDate;
 import java.time.Period;
 
@@ -7,14 +6,14 @@ import java.time.Period;
  * Created by roelg on 16/01/2017.
  */
 public abstract class Person {
-    private String name;
-    private LocalDate birthdate;
+    protected String name;
+    protected LocalDate birthdate;
+    protected boolean gender;
 
     public Person(String name, LocalDate birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
-
 
     public String getName() {
         return name;
@@ -23,6 +22,8 @@ public abstract class Person {
     public LocalDate getBirthdate() {
         return birthdate;
     }
+
+
 
     public int getAge(){
         return Period.between(this.birthdate, LocalDate.now()).getYears();
