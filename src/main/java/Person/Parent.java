1dffd49db6266162f05f0f8a52d36ad19fd15864
Roel Goossens <roelgoossens22@hotmail.com>
2017-01-18 16:02:49 +0100

    Added EmploymentType field

--- src/main/java/Person/Parent.java
+++ src/main/java/Person/Parent.java
@@ -8,13 +8,20 @@ import java.time.LocalDate;
  * Created by roelg on 18/01/2017.
  */
 public class Parent  extends Person{
+    public EmploymentType getEmploymentType() {
+        return employmentType;
+    }
 
     private EmploymentType employmentType;
 
-
-
-    public Parent(String name, LocalDate birthdate, Gender gender) {
+    public Parent(String name, LocalDate birthdate, Gender gender,EmploymentType employmentType) {
         super(name, birthdate,gender);
+        this.employmentType = employmentType;
+    }
+
+    public double getAddition(int amountParents, int amountChildren) {
+        // delegate
+        return employmentType.getAddition( amountParents,  amountChildren);
     }
 
 }
