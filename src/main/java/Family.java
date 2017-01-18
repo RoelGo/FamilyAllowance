d1ae0a33b6891deca58b3d34d2d2b719e3a4ce8d
Roel Goossens <roelgoossens22@hotmail.com>
2017-01-18 10:57:34 +0100

    Added lists within Family
    We are using arraylists to save childeren and parents

--- src/main/java/Family.java
+++ src/main/java/Family.java
@@ -1,7 +1,13 @@
 package main.java;
 
+import java.util.ArrayList;
 /**
  * Created by roelg on 18/01/2017.
  */
 public class Family {
+    ArrayList<Child> childeren = new ArrayList<>();
+    ArrayList<Parent> Parents = new ArrayList<>();
+
+
+
 }
